84988e42dd5e27351733c67208625d72808bdc57
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2024-05-31 23:35:27 +0600

    first commit

---
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,34 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
+  return (
+    <div>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Logged-in user email: {session?.user?.email}
+          </h1>
+          <Image
+            src={
+              session?.user?.image ||
+              "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
+            }
+            width={100}
+            height={100}
+            className="mx-auto rounded-full mt-5"
+            alt="user image"
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DashboardPage;
